bdf1af9cfbaf5e6631b245ba5621e98c3ddad8f7
DylanLouw <djlouw2005@gmail.com>
2026-07-17 09:32:48 +0200

    Added a fade in scroll animation aswell as card lift hover effect

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded",() => {
+    const sections = document.querySelectorAll("section");
+    sections.forEach(section => section.classList.add("reveal"));
+    const observer = new IntersectionObserver(
+        (entries) =>{
+            entries.forEach(entry => {
+             if(entry.isIntersecting){
+                 entry.target.classList.add("visible");
+             }
+            })
+        }, {threshold: 0.15}
+    );
+    sections.forEach(section => observer.observe(section));
+});
+
